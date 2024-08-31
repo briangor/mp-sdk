60900234ea3b9df91966b09708531a8a37ff6d72
briangor <briangor13@gmail.com>
2024-08-31 09:07:37 +0300

    add check for undefined process

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { setupSdk } from '@matterport/sdk';
 
+if (!process.env.SDK_KEY) {
+  console.error('SDK_KEY is not defined in the environment');
+}
+
 const main = async () => {
     const mpSdk = await setupSdk(process.env.SDK_KEY || 'SDK_KEY');
     await mpSdk.App.state.waitUntil(state => state.phase === mpSdk.App.Phase.PLAYING)
   mpSdk.Camera.rotate(35, 0)
   }
 main().catch(err => console.error('Error:', err));
-// todo: check node access to process variables
